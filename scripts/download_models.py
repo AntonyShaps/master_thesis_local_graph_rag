d1a592929c000ae2e94ab2cb0f507916a4060469
Anton Shapovalov <16188347+AntonyShaps@users.noreply.github.com>
2026-02-24 21:01:58 +0100

    refactor for easier upload of additional models

--- scripts/download_models.py
+++ scripts/download_models.py
@@ -1,28 +1,49 @@
 from paths import MODELS_DIR 
 from huggingface_hub import snapshot_download
+from dataclasses import dataclass
 
+@dataclass(frozen=True)
+class ModelSpec:
+    repo_id: str
+    local_dir: str
+    allow_patterns: list[str] | None = None
 
-nanonets_model = "nanonets/Nanonets-OCR2-3B"
-nanonets_dir = MODELS_DIR / "Nanonets-OCR2-3B"
-
-mistral_model = "mistralai/Mistral-7B-v0.3"
-mistral_dir = MODELS_DIR / "Mistral-7B-v0.3"
-
-if __name__ == "__main__":
-    if not nanonets_dir.exists() and not mistral_dir.exists():
-        nanonets_dir.mkdir(parents=True)
-        mistral_dir.mkdir(parents=True)
-        print(f"Created directories: \n{nanonets_dir}\n{mistral_dir}")
-    else:
-        print("Already there")
-
-    snapshot_download(repo_id=mistral_model,
-                      allow_patterns=["params.json",
-                                      "consolidated.safetensors",
-                                      "tokenizer.model.v3"],
-                      local_dir=mistral_dir
-                      )
-    snapshot_download(repo_id=nanonets_model,
-                      local_dir=nanonets_dir
-                      )
+MODELS: list[ModelSpec] = [
+        ModelSpec(
+            repo_id = "nanonets/Nanonets-OCR2-3B",
+            local_dir = MODELS_DIR / "Nanonets-OCR2-3B",
+            allow_patterns=["params.json",
+                            "consolidated.safetensors",
+                            "tokenizer.model.v3"]
+            ),
+        ModelSpec(
+            repo_id = "mistralai/Mistral-7B-v0.3",
+            local_dir = MODELS_DIR / "Mistral-7B-v0.3",
+            ),
+        ModelSpec(
+            repo_id ="lightonai/LightOnOCR-2-1B",
+            local_dir = MODELS_DIR / "LightOnOCR-2-1B"
+            )
+        ]
  
+def ensure_model_download(spec):
+    if spec.local_dir.exists():
+        print(f"{spec.repo_id} is already present at:\n{spec.local_dir}")
+        return spec.local_dir
+    spec.local_dir.mkdir(parents=True)
+    print(f"Dowloading: {spec.repo_id} -> {spec.local_dir}")
+
+    snapshot_download(
+        repo_id=spec.repo_id,
+        local_dir=spec.local_dir,
+        allow_patterns=spec.allow_patterns
+    )
+    print("Done!")
+    return spec.local_dir
+
+
+                         
+if __name__ == "__main__":
+    for spec in MODELS:
+        ensure_model_download(spec)
+
